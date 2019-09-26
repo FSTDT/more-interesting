9fc651253ce64feb6f2c1dd1df8c04292b5ab275
Michael Howell <michael@notriddle.com>
2019-09-25 22:33:56 -0700

    Legacy redirects are permanent
    I should've marked them as such the whole time.

--- src/main.rs
+++ src/main.rs
@@ -1368,7 +1368,7 @@ fn random(conn: MoreInterestingConn) -> Option<impl Responder<'static>> {
 
 #[get("/id/<id>")]
 fn redirect_legacy_id(id: i64) -> impl Responder<'static> {
-    Redirect::to(format!("/{}", Base32::from(id)))
+    Redirect::permanent(format!("/{}", Base32::from(id)))
 }
 
 #[get("/robots.txt")]
