46c10fa96779663f79c1a009cc595f18f3a55470
Michael Howell <michael@notriddle.com>
2019-12-22 17:03:08 +0000

    Update assets

--- assets/js/legacy.mjs
+++ assets/js/legacy.mjs
@@ -1,7 +1,7 @@
 import '@webcomponents/webcomponentsjs/webcomponents-bundle.js'
 import ExtraPolyfills from './extra-polyfills.mjs'
 import HTMLDetailsElement from 'details-element-polyfill'
-import DetailsDialogElement from 'details-dialog-element'
+import DetailsDialogElement from '@github/details-dialog-element'
 import DetailsMenuBarElement from './details-menu-bar.mjs'
 import AjaxFormElement from './ajax-form.mjs'
 import LocalDateElement from './local-date.mjs'
