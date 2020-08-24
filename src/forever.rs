fdc35f8fe613e20ef0b3bd650281e1e43ac4eb2c
Michael Howell <michael@notriddle.com>
2020-08-24 11:26:15 -0700

    Fix bug

--- src/forever.rs
+++ src/forever.rs
@@ -7,7 +7,7 @@ impl<'r, T: Responder<'r>> Responder<'r> for CacheForever<T> {
     fn respond_to(self, req: &Request) -> response::Result<'r> {
         let mut build = Response::build();
         build.merge(self.0.respond_to(req)?);
-        build.raw_header("cache-control", "cache-control: public, max-age=30000000");
+        build.raw_header("cache-control", "public, max-age=30000000");
         build.ok()
     }
 }
