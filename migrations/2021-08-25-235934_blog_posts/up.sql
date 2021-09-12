2a696dcd075fdbaea3ad0d9f3ed3962682142d24
Michael Howell <michael@notriddle.com>
2021-09-11 22:13:55 -0700

    Fix indexing

--- migrations/2021-08-25-235934_blog_posts/up.sql
+++ migrations/2021-08-25-235934_blog_posts/up.sql
@@ -1,4 +1,5 @@
 ALTER TABLE posts
   ADD COLUMN blog_post BOOL NOT NULL DEFAULT 'f';
 DROP INDEX idx_posts_homepage;
-CREATE INDEX idx_posts_homepage ON posts (blog_post, initial_stellar_time DESC, created_at DESC);
+CREATE INDEX idx_posts_blog_homepage ON posts (blog_post, initial_stellar_time DESC, created_at DESC);
+CREATE INDEX idx_posts_homepage ON posts (initial_stellar_time DESC, created_at DESC);
